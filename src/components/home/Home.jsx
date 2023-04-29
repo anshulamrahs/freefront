a49f4691676c33838d52d7abd8c2dc08d6ca340c
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-04-29 10:53:44 +0530

    hero page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,8 @@
 import './home.scss'
+import Male from '../assets/male.png'
+import Tex from '../assets/dd.png'
+import WE from '../assets/wire.jpeg'
+
 
 const Home = () => {
   return (
@@ -22,27 +26,42 @@ const Home = () => {
         </div>
         <div className="bottom">
             <div className="left">
-                <div className="l1">
+                <div className="top1">
+                    <div className="left2">
                 <h2>Hi! I am </h2>
                 <h2>Anshul Sharma</h2>
-                <div className="bt">Frontend</div>
+                    </div>
+                <div className="left3">
+                <span>Front-end</span>
                 </div>
-                <div className="btn">
-                    <button>Hire Me</button>
-                    <button>Projects</button>
                 </div>
-                <div className="he">
-                    <h1>84</h1>
-                    <span>clients worldwide</span>
-                    <h1>84</h1>
-                    <span>clients worldwide</span>
-                    <h1>84</h1>
-                    <span>clients worldwide</span>
+                <div className="btn-mid">
+                    <button className='hire'>Hire Me</button>
+                    <button className='bt'>Projects ↗</button>
                 </div>
-            </div>
-            <div className="right"></div>
+                <div className="bottom1">
+                    <div className="he1">
+                    <h1>+84</h1>
+                    <span className='sp'>clients on Upwork worldwide</span>
+                    <h1>572</h1>
+                    <span>Project Done</span>
+                    <h1>Contact</h1>
+                    <span>anshulamrahs@gmail.com</span>
+                    </div>
+                    <div className="he2">
+                    <img className='persons' src={Male} alt="" />
+                    
+                    <img src={Tex} className='per' alt=''/>
+    
         </div>
-    </div>
+                    </div>
+                </div>
+            <div className="right">
+                <img  className='dev' src={WE} alt></img>
+            </div>
+            </div>
+        </div>
+    
   )
 }
 
