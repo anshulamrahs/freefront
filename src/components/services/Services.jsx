443235652617f02463c7865671f4b31f889c1290
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-05-01 10:57:36 +0530

    services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import './services.scss'
+import Book from '../assets/sore.jpeg'
+import Mark from '../assets/mark.jpeg'
+import Strat from '../assets/strat.jpeg'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='service'>
+        <h1 className='tex'>Services Offered</h1>
+        <div className="serve">
+        <div className="serviceCard">
+            <img src={Book} className='img' alt='booking'/>
+            <h1>Web Development</h1>
+            <span>Full development Process from Planning to Deployment</span>
+        </div>
+        <div className="serviceCard">
+            <img src={Mark} className='img' alt='booking'/>
+            <h1>Digital Marketing</h1>
+            <span>Full Seo and Digital marketing Strategies for your website</span>
+        </div>
+        <div className="serviceCard">
+            <img src={Strat} className='img' alt='booking'/>
+            <h1>Business Strategies</h1>
+            <span>Strategies to grow your business from ground up</span>
+        </div>
+        </div>
+    </div>
   )
 }
 
